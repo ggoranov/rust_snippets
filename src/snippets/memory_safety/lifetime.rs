d5c2b57b76f9221498910fb35ba909fbd191b6de
Georgi Goranov <georgi.goranov3@gmail.com>
2019-01-14 18:55:50 +0200

    Borrow checker lifetime notations

--- src/snippets/memory_safety/lifetime.rs
+++ src/snippets/memory_safety/lifetime.rs
@@ -3,10 +3,16 @@
 // the reference it points at will also be 'alive' otherwise we can have
 // a dangling pointer "use after free".
 //
-// notation_id forces this restiction that Borrow checker promises to follow
-struct I32Wrapper<'notation_id> {
-    x: &'notation_id i32,
+// Lifetime notation 'a forces this restiction that Borrow checker promises to follow
+struct I32Wrapper<'a> {
+    x: &'a i32,
 }
+
+impl<'a> I32Wrapper<'a> {
+    fn set_value(&mut self, y: &'a i32) {
+        self.x = y;
+    }
+} 
 
 fn main() {
     {
@@ -16,10 +22,22 @@ fn main() {
 
         {
             let a: i32 = 111;               // -+ a goes into scope
-            wrapper = I32Wrapper { x: &a }  // Borrow checker error will occur if a or c are used
+            wrapper = I32Wrapper { x: &b }  // Borrow checker error will occur if a or c are used
         }                                   // ---+ a go out of scope
         
         let wrapper_ref: &I32Wrapper  = &wrapper;
         print!("Result: {}", wrapper_ref.x);
+    }
+
+    {
+        let b: i32 = 222; 
+        let wrapper = I32Wrapper { x: &b};
+        {
+            let a: i32 = 111;               // -+ a goes into scope
+            wrapper.set_value(&a);          // Borrow checker error will occur if a or c are used
+                            // ^ borrowed value does not live long enough
+        }                                   // ---+ a go out of scope
+        
+        print!("Result: {}", wrapper.x);
     }
 }
