9f31406577fb1cf2f0bc5092ae89d3dd7f09708e
Georgi Goranov <georgi.goranov3@gmail.com>
2019-01-15 11:04:32 +0200

    Borrowing and ownership samples

--- src/snippets/memory_safety/lifetime.rs
+++ src/snippets/memory_safety/lifetime.rs
@@ -3,7 +3,8 @@
 // the reference it points at will also be 'alive' otherwise we can have
 // a dangling pointer "use after free".
 //
-// Lifetime notation 'a forces this restiction that Borrow checker promises to follow
+// Lifetime notation 'a forces this restiction that Borrow checker promises
+// to check for us anytime I32Wrapper is used
 struct I32Wrapper<'a> {
     x: &'a i32,
 }
@@ -12,32 +13,51 @@ impl<'a> I32Wrapper<'a> {
     fn set_value(&mut self, y: &'a i32) {
         self.x = y;
     }
-} 
+}
 
 fn main() {
+    // Example 1
     {
-        let b: i32 = 222; 
-        let wrapper;                        // ---+ wrapper life start from here
-        let c: i32 = 333; 
+        let b: i32 = 222;
+        let wrapper; // ---+ wrapper life start from here
+        let c: i32 = 333;
 
         {
-            let a: i32 = 111;               // -+ a goes into scope
-            wrapper = I32Wrapper { x: &b }  // Borrow checker error will occur if a or c are used
-        }                                   // ---+ a go out of scope
-        
-        let wrapper_ref: &I32Wrapper  = &wrapper;
+            let a: i32 = 111; // -+ a goes into scope
+            wrapper = I32Wrapper { x: &b } // Borrow checker error will occur if a or c are used
+        } // ---+ a go out of scope
+
+        let wrapper_ref: &I32Wrapper = &wrapper;
         print!("Result: {}", wrapper_ref.x);
     }
 
+    // Example 2
     {
-        let b: i32 = 222; 
-        let wrapper = I32Wrapper { x: &b};
+        let b: i32 = 222;
+        let wrapper = I32Wrapper { x: &b };
         {
-            let a: i32 = 111;               // -+ a goes into scope
-            wrapper.set_value(&a);          // Borrow checker error will occur if a or c are used
-                            // ^ borrowed value does not live long enough
-        }                                   // ---+ a go out of scope
-        
+            let a: i32 = 111; // -+ a goes into scope
+            // wrapper.set_value(&a); // Borrow checker error will occur if a or c are used
+                                   // ^ borrowed value does not live long enough
+        } // ---+ a go out of scope
+
         print!("Result: {}", wrapper.x);
+    }
+
+    // Example 3
+    {
+        struct I32WrapperFields<'a> {
+            field1: &'a i32,
+            field2: &'a i32,
+        }
+
+        let x = 5;
+        {
+            let y = 10;
+            let f = I32WrapperFields {
+                field1: &x,
+                field2: &y,
+            };
+        }
     }
 }
--- src/snippets/memory_safety/ownership.rs
+++ src/snippets/memory_safety/ownership.rs
@@ -1,11 +1,45 @@
+// Move ownership is used so that only v1 (data_pointer, length val and capacity val) are only
+// copied to the caller stack frame but the allocated heap memory is not duplicated.
+#[allow(dead_code)]
+fn allocate_vec_heap_memory() -> (Vec<u32>, String) {
+    let mut v1 = vec![0u32; 1024 * 1024];
+    let addr_as_str = format!("{:?}", v1.as_mut_ptr());
+    (v1, addr_as_str)
+}
+
+fn read_vec_heap_memory() {
+    {
+        let (mut v2, addr_as_str) = allocate_vec_heap_memory(); // v2 gets the ownership of the allocated heap
+                                                                // and the responsibility to free memory when out of scope
+        let moved_addr_as_str = format!("{:?}", v2.as_mut_ptr());
+        assert_eq!(moved_addr_as_str, addr_as_str);
+    } // v2 is getting out of scope as a result allocated heap
+      // is freed and no mem leak occurs.
+}
+#[test]
+fn test_dynamic_memory() {
+    // Run with `valgrind --leak-check=full  ./main` to confirm lack of leaks.
+    read_vec_heap_memory();
+}
+
 fn main() {
+    sample_1();
+
+    sample_2();
+}
+
+fn sample_1() {
+    read_vec_heap_memory();
+    print!("read_vec_heap_memory done")
+}
+
+fn sample_2() {
     fn foo(v1: Vec<i32>, v2: Vec<i32>) -> (Vec<i32>, Vec<i32>, i32) {
         (v1, v2, 21)
     }
 
     let v1 = vec![1, 2, 3];
     let v2 = vec![3, 4, 5];
-
     /*
     stack memory:
     v1
@@ -23,6 +57,6 @@ fn main() {
 
     foo(v1, v2);
 
-    /// Cannot use them more: already moved
-    ///  print!("{:?}", v1);
+    // Cannot use them more: already moved
+    //  print!("{:?}", v1);}
 }
